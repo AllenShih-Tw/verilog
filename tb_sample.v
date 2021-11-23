1372ea3daab4b0bd48a19ef238bcc1947f4aecdd
Allen <allendxbest84@gmail.com>
2021-11-23 20:14:50 +0800

    edit tb_sample.v

--- tb_sample.v
+++ tb_sample.v
@@ -1,22 +1,26 @@
 module adder(
 input clk,
 input reset,
-input [7:0]a,
-input [7:0]b,
-output reg[7:0]c
+input [7:0]a_adder,
+input [7:0]b_adder,
+output reg[7:0]c_adder,
+output reg[7:0]d_adder
     ); 
  
  always@(posedge clk)begin
    if(reset)
-     c <= 0;
+     c_adder <= 0;
    else
-     c <= a + b;
+     c_adder <= a_adder + b_adder;
+ end
+ always@(negedge clk)begin
+   if(reset)
+     d_adder <= 0;
+   else
+     d_adder <= b_adder - a_adder;
  end
     
 endmodule
-
-
-
 
 
 module tb();
@@ -25,12 +29,14 @@ reg reset;
 reg[7:0] a;
 reg[7:0] b;
 wire[7:0] c;
+wire[7:0] d;
 adder uut(
   .clk(clk),
   .reset(reset),
-  .a(a),
-  .b(b),
-  .c(c)
+  .a_adder(a),
+  .b_adder(b),
+  .c_adder(c),
+  .d_adder(d)
 );
 initial begin
 
@@ -41,11 +47,11 @@ initial begin
   #10;
   reset = 1 ;
   #10;
-  reset = 1;
+  reset = 0;
   #100;
   a = 4;
   b = 7;
-  #10;
+  #100;
   a = 8;
   b = 12;
 end
